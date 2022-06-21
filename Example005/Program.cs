ab52c55692874456011d9b14364f883c8690918b
Anatoly Finchenko <finchenko777@gmail.com>
2022-06-21 21:35:25 +0300

    *ex005*

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя : ") ;
+string username = Console.ReadLine() ;
+
+ if (username == "Маша" ) 
+ {
+     Console.WriteLine("Поздравляю Маша !!! ") ;
+ }
+ else
+ {
+    Console.Write("Привет " + (username) + " !!!") ;
+ }
